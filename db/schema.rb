24f1f0dec76d8876dfefb2f6a9073514097970ae
Daniel Yu <dcyu93@gmail.com>
2015-11-04 08:38:04 -0500

    phone number field added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031231711) do
+ActiveRecord::Schema.define(version: 20151104133103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151031231711) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "organization"
+    t.string   "phone"
   end
 
   create_table "shifts", force: :cascade do |t|
